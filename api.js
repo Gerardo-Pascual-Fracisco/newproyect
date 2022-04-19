abf5b5718c9f21ddb8c59a2a9f9cf3f40dc1542a
Darkneko0 <al221911729@gmail.com>
2022-04-19 18:16:10 -0500

    obtener ID categoria

--- api.js
+++ api.js
@@ -1,37 +1,44 @@
 
-const API = 'http://192.168.201.27:8000/api/category'
-const SERVICES = 'http://192.168.201.27:8000/api/service'
-const MOSTRAR_SERVICES = 'http://192.168.201.27:8000/api/showById/1'
 
+const API = 'http://192.168.1.72:8000/api/category'
+const SERVICES = 'http://192.168.1.72:8000/api/service'
 
-export const getTasks=async() => { 
-    const res = await fetch(API)
-    return await res.json()
+const MOSTRAR_SERVICES = 'http://192.168.92.27:8000/api/showById'
+
+//obtener categorias
+export const getTasks = async () => {
+  const res = await fetch(API)
+  return await res.json();
 }
-export const getServices=async() => { 
+//obtener servicios
+export const getServices = async () => {
   const res = await fetch(SERVICES)
-  return await res.json()
+  return await res.json();
 }
 
-export const getCateServices=async() => { 
-  const res = await fetch(MOSTRAR_SERVICES)
-  return await res.json()
-}
+
+//obtener servicios de categoria
+export const getCateServices = async (id) => {
+  const res = await fetch(`${MOSTRAR_SERVICES}/${id}`);
+  return await res.json();
+};
+
 export const saveTask = async (newTask) => {
-    const res = await fetch(API, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTask),
-    });
-    return await res.json();
-  };
-  
-export const deleteTask = async (id_category) => {
-    await fetch(`${API}/${id_category}`, {
-      method: "DELETE",
-    });
+  const res = await fetch(API, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTask),
+  });
+  return await res.json();
+};
 
-  };
+//eliminar categoria
+export const deleteTask = async (id_category) => {
+  await fetch(`${API}/${id_category}`, {
+    method: "DELETE",
+  });
+
+};
--- components/Categories/CategoryServices.js
+++ components/Categories/CategoryServices.js
@@ -1,10 +1,81 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-export default function CategoryServices ({ navigation }) {
-    return (
-         <View>
-             <Text>Services...</Text>
-         </View>
-         )
-        }
-const styles = StyleSheet.create({})
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, FlatList, RefreshControl } from 'react-native'
+const MOSTRAR_SERVICES = 'http://192.168.1.72:8000/api/showById'
+
+export default function CategoryServices({ navigation, route }) {
+
+  const { name, id } = route.params
+  const id_category=id;
+  navigation.setOptions({ title: name })
+
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [tasks, setTasks] = useState([])
+
+  //obtener servicios de categoria
+  const getCateServices = async (id=id_category) => {
+    const res = await fetch(`${MOSTRAR_SERVICES}/${id}`);
+    console.log(`${MOSTRAR_SERVICES}/${id}`);
+    return await res.json();
+  };
+
+  const loadTasks = async () => {
+    const data = await getCateServices()
+    setTasks(data.services)
+    console.log(data.services);
+  };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  const renderItem = ({ item }) => {
+<View><Text>{item.foto}</Text></View>
+  };
+
+  const onRefresh = React.useCallback(async () => {
+
+    setRefreshing(true);
+    await loadTasks();
+    setRefreshing(false);
+  })
+  return (
+    <FlatList
+      style={{ width: '100%' }}
+      data={tasks}
+      keyExtractor={(item) => item.id_service + ""}
+      renderItem={renderItem}
+
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          colors={["#78e08f"]}
+          onRefresh={onRefresh}
+          progressBackgroundColor="#000"
+        />
+      }
+    />
+
+  );
+};
+const styles = StyleSheet.create({
+  itemContainer: {
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+    marginVertical: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  itemTitle: {
+    color: "#000000",
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+});
+
+
+
+
